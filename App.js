3438919b4c7a917609c3f8f500a2658daf97905a
Marcus Nguyen <marcus.nguyen@grapecity.com>
2021-12-02 17:56:21 +0700

    Add tabbar to app

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { SafeAreaView, StyleSheet, StatusBar } from "react-native";
+import { SafeAreaView, StyleSheet } from "react-native";
 import * as Font from "expo-font";
 import AppLoading from "expo-app-loading";
 import { enableScreens } from "react-native-screens";
