da3b8d97ded98156754f611d96a499c952514092
Marcus Nguyen <marcus.nguyen@grapecity.com>
2021-12-02 09:40:44 +0700

    Add prettier

--- App.js
+++ App.js
@@ -1,12 +1,12 @@
-import React, { useState } from 'react';
-import { Text, View } from 'react-native';
-import * as Font from 'expo-font';
-import AppLoading from 'expo-app-loading';
+import React, { useState } from "react";
+import { Text, View } from "react-native";
+import * as Font from "expo-font";
+import AppLoading from "expo-app-loading";
 
 const fetchFonts = () => {
   return Font.loadAsync({
-    'open-sans': require('./assets/fonts/OpenSans-Regular.ttf'),
-    'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf')
+    "open-sans": require("./assets/fonts/OpenSans-Regular.ttf"),
+    "open-sans-bold": require("./assets/fonts/OpenSans-Bold.ttf"),
   });
 };
 
