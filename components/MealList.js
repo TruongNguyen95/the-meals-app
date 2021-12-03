01cdcf300a32ef5d77939d1204a5a79730f8640e
Marcus Nguyen <marcus.nguyen@grapecity.com>
2021-12-03 18:57:35 +0700

    Add redux

--- components/MealList.js
+++ components/MealList.js
@@ -1,9 +1,16 @@
 import React from "react";
 import { View, FlatList, StyleSheet } from "react-native";
-import MealItem from "../components/MealItem";
+import { useSelector } from "react-redux";
+
+import MealItem from "./MealItem";
 
 const MealList = (props) => {
+  const favoriteMeals = useSelector((state) => state.meals.favoriteMeals);
+
   const renderMealItem = (itemData) => {
+    const isFavorite = favoriteMeals.some(
+      (meal) => meal.id === itemData.item.id
+    );
     return (
       <MealItem
         title={itemData.item.title}
@@ -12,8 +19,13 @@ const MealList = (props) => {
         complexity={itemData.item.complexity}
         affordability={itemData.item.affordability}
         onSelectMeal={() => {
-          props.navigation.navigate("MealDetail", {
-            mealId: itemData.item.id,
+          props.navigation.navigate({
+            routeName: "MealDetail",
+            params: {
+              mealId: itemData.item.id,
+              mealTitle: itemData.item.title,
+              isFav: isFavorite,
+            },
           });
         }}
       />
